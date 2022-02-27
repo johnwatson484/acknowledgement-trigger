064e758373750363f5cfeba60002ddcdf489da77
John Watson <johnwatson484@hotmail.com>
2022-02-27 17:30:47 +0000

    Create trigger

--- acknowledgement-trigger/index.js
+++ acknowledgement-trigger/index.js
@@ -1,3 +1,21 @@
-module.exports = async function(context, mySbMsg) {
-    context.log('JavaScript ServiceBus topic trigger function processed message', mySbMsg);
-};
+const axios = require('axios')
+const CONTAINER_NAME = 'target'
+
+module.exports = async function (context, mySbMsg) {
+  context.log('JavaScript ServiceBus topic trigger function processed message', mySbMsg)
+
+  const sourceFile = mySbMsg.acknowledgementFile.replace(`${CONTAINER_NAME}/`, '')
+
+  const result = await axios.post(process.env.TRIGGER_URL, {
+    sourceFile,
+    sourceContainer: CONTAINER_NAME,
+    targetContainer: 'batch',
+    targetFolder: 'inbound'
+  })
+
+  if (result.status === 200) {
+    console.log(`Successfully triggered transfer for ${sourceFile}`)
+  } else {
+    console.log(`Unable to transfer ${sourceFile}`)
+  }
+}
